0c0e2e1c2c9b657f964bcca031c5d033384dde8b
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-03-30 11:03:28 +0000

    feat: Implement draggable VsCode window with minimize/close actions

---
+++ src/components/VsCode.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import Draggable from "react-draggable";
+
+function VsCode({ isAppOpen, toggleVsCode }) {
+  const [contentLoaded, setContentLoaded] = useState(false);
+
+  useEffect(() => {
+    if (isAppOpen && !contentLoaded) {
+      setContentLoaded(true);
+    }
+  }, [isAppOpen, contentLoaded]);
+
+  return (
+    <>
+      <div className={`${isAppOpen ? "" : "hidden"} absolute z-30`}>
+        <Draggable handle=".title-bar">
+          <div className="window bg-black h-[45rem] w-[70.5rem] rounded-xl overflow-hidden border-neutral-700 border-[1.5px] font-semibold">
+            <div className="title-bar">
+              <div className="text-white h-9 flex justify-between select-none">
+                <div className="m-1 ml-4 font-normal">Visual Studio Code</div>
+                <div className="flex">
+                  <div
+                    className="material-symbols-outlined hover:bg-neutral-800 mb-2 w-11 flex justify-center items-center text-xl"
+                    onClick={toggleVsCode}
+                  >
+                    minimize
+                  </div>
+                  <div className="material-symbols-outlined hover:bg-neutral-800 mb-2 w-11 flex justify-center items-center text-sm">
+                    check_box_outline_blank
+                  </div>
+                  <div
+                    className="material-symbols-outlined hover:bg-red-700 mb-2 w-12 flex justify-center items-center text-xl"
+                    onClick={toggleVsCode}
+                  >
+                    close
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="content text-white select-none text-center flex justify-center h-full">
+              {contentLoaded && (
+                <iframe
+                  src="https://github1s.com/MishanPoudel/Windows11-3.0/blob/main/src/Pages/main.js"
+                  title="VsCode"
+                  className="h-full w-full bg-ub-cool-grey"
+                ></iframe>
+              )}
+            </div>
+          </div>
+        </Draggable>
+      </div>
+    </>
+  );
+}
+
+export default VsCode;
