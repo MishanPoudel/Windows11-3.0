eb87089fa12e2e78eb92e8c80029cc583a9d3816
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-03-30 11:04:34 +0000

    feat: Implement lockscreen with login and sliding menu

--- src/Pages/lockscreen.js
+++ src/Pages/lockscreen.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "../components/Login";
+import Slider from "../components/Slider";
 
 function Lockscreen() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <div
         className="absolute bg-black h-screen w-full blur-sm"
         style={{
-          background:
-            "url(https://img.wallpapersafari.com/desktop/1440/900/11/85/Ra8DH9.jpg)",
+          background: "url(https://images8.alphacoders.com/134/1346089.png)",
           backgroundSize: "100% 100%",
         }}
       ></div>
       <div className="absolute left-0 top-0 h-screen w-full flex flex-col items-center z-10">
         <Login />
       </div>
+      <Slider
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+        toggleMenu={toggleMenu}
+      />
     </>
   );
 }
