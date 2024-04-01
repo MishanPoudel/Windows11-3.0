bd52378465e692127a34e0af1d0c3ea3b16feb10
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-04-01 20:01:44 +0100

    wip: Implement Explorer component (structure + partial functionality)

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,11 +1,11 @@
 import React, { useRef } from "react";
 import Draggable from "react-draggable";
 
-const Explorer = ({isExplorerOpen, toggleExplorer}) => {
+const Explorer = ({ isExplorerOpen, toggleExplorer }) => {
   const explorerRef = useRef(null);
 
   return (
-    <div className={`${isExplorerOpen?"":"hidden"} z-30`}>
+    <div className={`${isExplorerOpen ? "" : "hidden"} z-30`}>
       <Draggable handle=".title-bar" nodeRef={explorerRef}>
         <div
           ref={explorerRef}
@@ -19,15 +19,18 @@ const Explorer = ({isExplorerOpen, toggleExplorer}) => {
               <div className="material-symbols-outlined hover:bg-neutral-800 mb-2 w-11 flex justify-center items-center text-sm">
                 check_box_outline_blank
               </div>
-              <div className="material-symbols-outlined hover:bg-red-700 mb-2 w-12 flex justify-center items-center text-xl" onClick={toggleExplorer}>
+              <div
+                className="material-symbols-outlined hover:bg-red-700 mb-2 w-12 flex justify-center items-center text-xl"
+                onClick={toggleExplorer}
+              >
                 close
               </div>
             </div>
           </div>
-          <div className="content text-white select-none text-center">
+          <div className="content text-white select-none">
             <div className="absolute bg-neutral-800 top-[6.5px] h-[2em] left-[6px] w-60 rounded-t-lg flex">
               <div className="flex justify-between items-center w-full">
-                <div className="pl-2 text-sm">this iswhat it is</div>
+                <div className="pl-2 text-sm">Home</div>
                 <div className="material-symbols-outlined hover:bg-neutral-800 m-0.5 w-6 rounded-md flex justify-center items-center text-lg">
                   close
                 </div>
@@ -36,11 +39,134 @@ const Explorer = ({isExplorerOpen, toggleExplorer}) => {
                 add
               </div>
             </div>
-            <div className="bg-neutral-800 w-full h-12 border-neutral-700 border-b-[1.5px] mt-1">
-              jhsdf
+            <div className="bg-neutral-800 w-full h-12 border-neutral-700 border-b-[1.5px] mt-1 flex">
+              <div className="flex justify-around w-48 py-2">
+                <div class="material-symbols-outlined font-extralight text-xl opacity-45">
+                  arrow_back
+                </div>
+                <div class="material-symbols-outlined font-extralight text-xl opacity-45">
+                  arrow_forward
+                </div>
+                <div class="material-symbols-outlined font-extralight text-xl hover:bg-neutral-600 rounded-md hover:bg-opacity-50">
+                  arrow_upward
+                </div>
+                <div class="material-symbols-outlined font-extralight text-xl hover:bg-neutral-600 rounded-md hover:bg-opacity-50">
+                  refresh
+                </div>
+              </div>
+              <div className="flex bg-neutral-700 bg-opacity-50 my-1.5 rounded-md items-center text-sm px-2 mx-2 flex-grow gap-2 font-extralight">
+                <div class="material-symbols-outlined font-extralight">
+                  home
+                </div>
+                <div class="material-symbols-outlined font-extralight">
+                  navigate_next
+                </div>
+                <div>Home</div>
+                <div class="material-symbols-outlined font-extralight">
+                  navigate_next
+                </div>
+              </div>
+              <div className="flex justify-between bg-neutral-700 bg-opacity-50 my-1.5 rounded-md items-center text-sm px-4 mr-3 w-[19.3em]">
+                <div className="opacity-80">Search Home</div>
+                <div class="material-symbols-outlined font-extralight text-sm">
+                  search
+                </div>
+              </div>
             </div>
-            <div className="bg-neutral-900 w-full h-[3.4rem] border-neutral-700 border-b-[1.5px]">
-              jhsdf
+            <div className="bg-neutral-900 w-full h-[3.4rem] border-neutral-700 border-b-[1.5px] flex justify-between">
+              <div className="flex items-center">
+                <div className="flex items-center justify-center w-24 h-full text-xs gap-1 border-neutral-700 border-r-[1.5px] opacity-45">
+                  <img
+                    src="/images/icons/new.png"
+                    alt="new"
+                    className="w-5 h-5"
+                  />
+                  New
+                  <div className="material-symbols-outlined text-sm">
+                    expand_more
+                  </div>
+                </div>
+                <div className="flex h-full w-72 justify-around items-center border-neutral-700 border-r-[1.5px] opacity-45">
+                  <img
+                    src="/images/icons/cut.png"
+                    alt="cut"
+                    className="h-5 w-5"
+                  />
+                  <img
+                    src="/images/icons/copy.png"
+                    alt="copy"
+                    className="h-5 w-5"
+                  />
+                  <img
+                    src="/images/icons/paste.png"
+                    alt="paste"
+                    className="h-7 w-7"
+                  />
+                  <img
+                    src="/images/icons/rename.png"
+                    alt="rename"
+                    className="h-5 w-5"
+                  />
+                  <img
+                    src="/images/icons/share.png"
+                    alt="share"
+                    className="h-5 w-5"
+                  />
+                  <img
+                    src="/images/icons/delete.png"
+                    alt="delete"
+                    className="h-5 w-5"
+                  />
+                </div>
+                <div className="flex h-full items-center w-72 justify-around border-neutral-700 border-r-[1.5px]">
+                  <div className="flex items-center justify-center h-full text-xs gap-1 opacity-45">
+                    <img
+                      src="/images/icons/sort.png"
+                      alt="sort"
+                      className="w-5 h-5"
+                    />
+                    Sort
+                    <div className="material-symbols-outlined text-sm">
+                      expand_more
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-center h-full text-xs gap-1">
+                    <img
+                      src="/images/icons/view.png"
+                      alt="view"
+                      className="w-5 h-5"
+                    />
+                    View
+                    <div className="material-symbols-outlined text-sm">
+                      expand_more
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-center h-full text-xs gap-1">
+                    <img
+                      src="/images/icons/filter.png"
+                      alt="filter"
+                      className="w-5 h-5"
+                    />
+                    Filter
+                    <div className="material-symbols-outlined text-sm">
+                      expand_more
+                    </div>
+                  </div>
+                </div>
+                <img
+                  src="/images/icons/dots.png"
+                  alt="dots"
+                  className="w-3.5 h-3.5 ml-4"
+                />
+              </div>
+              <div className="flex items-center mr-8 text-xs">
+                <img
+                  src="/images/icons/details.png"
+                  alt="details"
+                  className="w-5 h-5 mr-1"
+                />
+                Details
+              </div>
             </div>
             <div className="flex flex-row h-full bg-neutral-900">
               <div className="w-40 h-[100vh] pt-2 border-neutral-700 border-r-[1.5px]">
