15fe8db1011583c6b22768dc04d474ac11d5daf4
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-06-23 20:05:04 +0100

    wip: Initial implementation of right-click context menu component

--- src/components/RightClick.jsx
+++ src/components/RightClick.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function RightClick() {
+function RightClick({ option }) {
   const [contextMenuPosition, setContextMenuPosition] = useState({
     x: 0,
     y: 0,
@@ -26,35 +26,126 @@ function RightClick() {
           className="context-menu"
           style={{ left: contextMenuPosition.x, top: contextMenuPosition.y }}
         >
-          <div className="w-[21rem] bg-neutral-800 border-[1px] border-neutral-700 rounded-xl p-1.5">
-            <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center">
-              <div></div>
-              <div className="justify-center">View</div>
-              <div className="material-symbols-outlined rotate-[-90deg] justify-end">
-                expand_more
-              </div>
-            </div>
-            <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center">
-              sort by
-            </div>
-            <div
-              className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center"
-              onClick={handleClick}
-            >
-              refresh
-            </div>
-            <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center">
-              new
-            </div>
-            <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center">
-              display setting
-            </div>
-            <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center">
-              personalize
-            </div>
-            <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center">
-              show more options
-            </div>
+          <div className="w-[15rem] bg-neutral-800 border-[1px] border-neutral-700 rounded-xl p-1.5">
+            {option ? (
+              <>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">View</div>
+                  </div>
+                  <div className="material-symbols-outlined rotate-[-90deg] justify-end opacity-50 font-extralight">
+                    expand_more
+                  </div>
+                </div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Sort by</div>
+                  </div>
+                  <div className="material-symbols-outlined rotate-[-90deg] justify-end opacity-50 font-extralight">
+                    expand_more
+                  </div>
+                </div>
+                <div
+                  className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1"
+                  onClick={handleClick}
+                >
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Refresh</div>
+                  </div>
+                </div>
+                <div className="divider m-0"></div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">New</div>
+                  </div>
+                  <div className="material-symbols-outlined rotate-[-90deg] justify-end opacity-50 font-extralight">
+                    expand_more
+                  </div>
+                </div>
+                <div className="divider m-0"></div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Display Settings</div>
+                  </div>
+                </div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Personalize</div>
+                  </div>
+                </div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Show More Options</div>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">View</div>
+                  </div>
+                  <div className="material-symbols-outlined rotate-[-90deg] justify-end opacity-50 font-extralight">
+                    expand_more
+                  </div>
+                </div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Sort by</div>
+                  </div>
+                  <div className="material-symbols-outlined rotate-[-90deg] justify-end opacity-50 font-extralight">
+                    expand_more
+                  </div>
+                </div>
+                <div
+                  className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1"
+                  onClick={handleClick}
+                >
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Refresh</div>
+                  </div>
+                </div>
+                <div className="divider m-0"></div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">New</div>
+                  </div>
+                  <div className="material-symbols-outlined rotate-[-90deg] justify-end opacity-50 font-extralight">
+                    expand_more
+                  </div>
+                </div>
+                <div className="divider m-0"></div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Display Settings</div>
+                  </div>
+                </div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Personalize</div>
+                  </div>
+                </div>
+                <div className="hover:bg-neutral-700 rounded-md whitespace-nowrap w-full h-7 select-none flex items-center justify-between px-1">
+                  <div>
+                    <div></div>
+                    <div className="justify-center">Show More Options</div>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
       )}
