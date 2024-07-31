1149c3bec4683788787a5cf453a7a4d7657a4d5d
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-07-31 22:36:04 +0100

    refactor: Optimize Lockscreen component using useCallback

--- src/Pages/lockscreen.js
+++ src/Pages/lockscreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Login from "../components/user/Login";
 import Slider from "../components/utilities/Slider";
 import MobileDetection from "../components/utilities/MobileDetection";
@@ -7,35 +7,45 @@ function Lockscreen() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  // Use useCallback to memoize the function and avoid unnecessary re-renders
+  const toggleMenu = useCallback(() => {
+    setIsMenuOpen((prev) => !prev);
+  }, []);
 
-  const handleMobileDetection = (mobile) => {
+  // Callback for mobile detection
+  const handleMobileDetection = useCallback((mobile) => {
     setIsMobile(mobile);
-  };
+  }, []);
 
+  // Conditional rendering for mobile detection
   if (isMobile) {
     return (
       <div className="bg-black w-full h-screen text-center text-3xl px-7 overflow-hidden flex flex-col justify-center items-center">
-        😔<br/> Sorry, this app is not supported on mobile devices <div className="font-bold">YET.</div> 🙏
+        😔
+        <br />
+        Sorry, this app is not supported on mobile devices{" "}
+        <div className="font-bold">YET.</div> 🙏
       </div>
     );
   }
 
   return (
     <>
-      <MobileDetection onDetectMobile={handleMobileDetection} />
+      {!isMobile && <MobileDetection onDetectMobile={handleMobileDetection} />}
+
       <div
         className="absolute bg-black h-screen w-full blur-sm"
         style={{
-          background: "url(https://images8.alphacoders.com/134/1346089.png)",
-          backgroundSize: "100% 100%",
+          background:
+            "url(https://images8.alphacoders.com/134/1346089.png) no-repeat center center",
+          backgroundSize: "cover",
         }}
       ></div>
+
       <div className="absolute left-0 top-0 h-screen w-full flex flex-col items-center z-10">
         <Login />
       </div>
+
       <Slider
         isMenuOpen={isMenuOpen}
         setIsMenuOpen={setIsMenuOpen}
