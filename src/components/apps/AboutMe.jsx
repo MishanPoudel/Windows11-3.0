db086fcd1c36312aa090a9debd045e8d61354ed1
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-07-31 22:25:55 +0100

    feat: Add skills section to AboutMe component

--- src/components/apps/AboutMe.jsx
+++ src/components/apps/AboutMe.jsx
@@ -7,14 +7,14 @@ import {
 } from "../../data/data";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
-const SkillItem = ({ skillItem, isTechStack = false }) => {
+const SkillItem = ({ skillItem, isTechStack = false, iconSize = 15 }) => {
   if (!skillItem || !skillItem.icon) {
     return null;
   }
 
   return (
     <div className="flex items-center ring-2 ring-neutral-700 bg-neutral-900 rounded-sm p-2 pl-3">
-      {skillItem.icon}{" "}
+      {React.cloneElement(skillItem.icon, { size: iconSize })}
       <span
         className={`ml-2 text-neutral-400 text-selection hover:text-neutral-200 duration-150 ease-in-out cursor-pointer ${
           isTechStack ? "text-xs" : "text-sm"
@@ -27,12 +27,11 @@ const SkillItem = ({ skillItem, isTechStack = false }) => {
 };
 
 const ProjectCard = ({ repo }) => {
-  const renderSkills = () => {
-    return repo.techUsed.map((tech, index) => {
-      const techSkill = skills.filter((skill) => skill.name === tech)[0];
+  const renderSkills = () =>
+    repo.techUsed.map((tech, index) => {
+      const techSkill = skills.find((skill) => skill.name === tech);
       return <SkillItem skillItem={techSkill} isTechStack={true} key={index} />;
     });
-  };
 
   return (
     <div className="bg-neutral-900/80 rounded-md px-4 pt-3 hover:translate-x-1 hover:-translate-y-1 duration-300 text-selection">
@@ -63,128 +62,238 @@ const ProjectCard = ({ repo }) => {
   );
 };
 
-function AboutMe({ page }) {
+const Skill = ({ icon, name, size }) => (
+  <div
+    className={`w-[${
+      size === 48 ? "6em" : "5em"
+    }] h-24 flex flex-col justify-center items-center rounded-md hover:bg-white hover:bg-opacity-20 p-2`}
+  >
+    {React.cloneElement(icon, { size })}
+    <div className="text-balance text-center text-sm select-none pt-2">
+      {name}
+    </div>
+  </div>
+);
+
+const SkillsList = ({ x, y }) => (
+  <div className="flex flex-wrap gap-2">
+    <>
+      {skills.slice(x, y).map((skill) => (
+        <Skill key={skill.key} icon={skill.icon} name={skill.name} size={48} />
+      ))}
+    </>
+  </div>
+);
+
+const AboutMe = ({ page, handleDivClick, expandedDiv }) => {
+  const renderPageContent = () => {
+    switch (page) {
+      case "About Me":
+        return (
+          <div className="hero min-h-auto justify-start">
+            <div className="hero-content flex-col lg:flex-row">
+              <img
+                src="https://i.pinimg.com/564x/05/6c/65/056c65643e34cb3927358b2c031b7c05.jpg"
+                className="max-w-sm rounded-lg shadow-2xl h-96 w-96"
+                alt="Profile"
+              />
+              <div>
+                <h1 className="text-5xl font-bold">About Me</h1>
+                <p className="py-6">{profileDescription}</p>
+              </div>
+            </div>
+          </div>
+        );
+      case "Education":
+        return (
+          <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical my-8">
+            <li>
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-start md:text-end mb-10">
+                <time className="font-mono text-lg italic">
+                  {educationExperience[1].graduation}
+                </time>
+                <div className="text-xl font-bold font-3xl">
+                  {educationExperience[1].institution}
+                </div>
+                {educationExperience[1].degree}
+              </div>
+              <hr className="bg-gray-500" />
+            </li>
+            <li>
+              <hr className="bg-gray-500" />
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end mb-10">
+                <time className="font-mono text-lg italic">
+                  {educationExperience[0].graduation}
+                </time>
+                <div className="text-xl font-bold font-3xl">
+                  {educationExperience[0].institution}
+                </div>
+                {educationExperience[0].degree}
+              </div>
+              <hr className="bg-gray-500" />
+            </li>
+            <div className="mx-auto">???</div>
+          </ul>
+        );
+      case "Skills":
+        return (
+          <div className="main-container flex h-screen relative">
+            {expandedDiv === 0 && (
+              <>
+                <div
+                  className="w-[5em] h-28 flex flex-col pt-2 items-center rounded-md hover:bg-white hover:bg-opacity-20"
+                  onDoubleClick={() => handleDivClick(1)}
+                >
+                  <img
+                    src="/images/apps/folder.png"
+                    alt="Technical"
+                    className="w-12 h-12"
+                  />
+                  <div className="text-balance text-center text-sm select-none pt-2">
+                    Technical Skills
+                  </div>
+                </div>
+
+                <div
+                  className="w-[5em] h-28 flex flex-col pt-2 items-center rounded-md hover:bg-white hover:bg-opacity-20"
+                  onDoubleClick={() => handleDivClick(2)}
+                >
+                  <img
+                    src="/images/apps/folder.png"
+                    alt="Soft"
+                    className="w-12 h-12"
+                  />
+                  <div className="text-balance text-center text-sm select-none pt-2">
+                    Soft Skills
+                  </div>
+                </div>
+
+                <div
+                  className="w-[5em] h-28 flex flex-col pt-2 items-center rounded-md hover:bg-white hover:bg-opacity-20"
+                  onDoubleClick={() => handleDivClick(3)}
+                >
+                  <img
+                    src="/images/apps/folder.png"
+                    alt="Design"
+                    className="w-12 h-12"
+                  />
+                  <div className="text-balance text-center text-sm select-none pt-2">
+                    Design Skills
+                  </div>
+                </div>
+              </>
+            )}
+
+            {expandedDiv === 1 && (
+              <div className="flex absolute top-0 gap-2">
+                <SkillsList x={0} y={17} />
+              </div>
+            )}
+
+            {expandedDiv === 2 && (
+              <div className="flex absolute top-0 gap-1">
+                <div className="w-[6.5em] h-28 flex flex-col pt-2 items-center rounded-md hover:bg-white hover:bg-opacity-20">
+                  <img
+                    src="/images/folders/communication.png"
+                    alt="Communication"
+                    className="w-12 h-12"
+                  />
+                  <div className="text-pretty text-center text-sm select-none pt-2">
+                    Communication
+                  </div>
+                </div>
+                <div className="w-[6em] h-28 flex flex-col pt-2 items-center rounded-md hover:bg-white hover:bg-opacity-20">
+                  <img
+                    src="/images/folders/teamwork.png"
+                    alt="Teamwork"
+                    className="w-12 h-12"
+                  />
+                  <div className="text-pretty text-center text-sm select-none pt-2">
+                    Teamwork
+                  </div>
+                </div>
+                <div className="w-[5em] h-28 flex flex-col pt-2 items-center rounded-md hover:bg-white hover:bg-opacity-20">
+                  <img
+                    src="/images/folders/problem.png"
+                    alt="Problem"
+                    className="w-12 h-12"
+                  />
+                  <div className="text-pretty text-center text-sm select-none pt-2">
+                    Problem Solving
+                  </div>
+                </div>
+                <div className="w-[6em] h-28 flex flex-col justify-center items-center rounded-md hover:bg-white hover:bg-opacity-20">
+                  <img
+                    src="/images/folders/management.png"
+                    alt="Project"
+                    className="w-12 h-12"
+                  />
+                  <div className="text-pretty text-center text-sm select-none pt-2">
+                    Project Management
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {expandedDiv === 3 && (
+              <div className="flex absolute top-0 gap-2">
+                <SkillsList x={17} y={20} />
+              </div>
+            )}
+          </div>
+        );
+      case "My Stuffs":
+        return (
+          <div>
+            <div className="grid sm:grid-cols-2 gap-2">
+              {githubRepos.map((repo, index) => (
+                <ProjectCard key={index} repo={repo} />
+              ))}
+            </div>
+          </div>
+        );
+      case "Resume":
+        return (
+          <main className="border-0 flex w-full justify-center opacity-75 mt-2 text-sm">
+            too bored to make a resume.
+          </main>
+        );
+      default:
+        return "404 not found";
+    }
+  };
+
   return (
-    <main className="h-[100vh] w-full ml-2.5 mt-2">
-      {page === "About Me" ? (
-        <div className="hero min-h-auto justify-start">
-          <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="https://i.pinimg.com/564x/05/6c/65/056c65643e34cb3927358b2c031b7c05.jpg"
-              className="max-w-sm rounded-lg shadow-2xl h-96 w-96"
-              alt="Profile"
-            />
-            <div>
-              <h1 className="text-5xl font-bold">About Me</h1>
-              <p className="py-6">{profileDescription}</p>
-            </div>
-          </div>
-        </div>
-      ) : page === "Education" ? (
-        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical my-8">
-          <li>
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-start md:text-end mb-10">
-              <time className="font-mono text-lg italic">
-                {educationExperience[1].graduation}
-              </time>
-              <div className="text-xl font-bold font-3xl">
-                {educationExperience[1].institution}
-              </div>
-              {educationExperience[1].degree}
-            </div>
-            <hr className="bg-gray-500" />
-          </li>
-          <li>
-            <hr className="bg-gray-500" />
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-end mb-10">
-              <time className="font-mono text-lg italic">
-                {educationExperience[0].graduation}
-              </time>
-              <div className="text-xl font-bold font-3xl">
-                {educationExperience[0].institution}
-              </div>
-              {educationExperience[0].degree}
-            </div>
-            <hr className="bg-gray-500" />
-          </li>
-          <div className="mx-auto">???</div>
-        </ul>
-      ) : page === "Skills" ? (
-        <section className="skills-section p-8">
-          <h2 className="text-2xl font-bold mb-4">Skills</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div>
-              <h3 className="text-xl mb-2">Technical Skills</h3>
-              <ul className="list-disc list-inside">
-                <li>Python, JavaScript, Java</li>
-                <li>React, Node.js, Django</li>
-                <li>MySQL, MongoDB</li>
-                <li>Docker, Kubernetes</li>
-                <li>AWS, GCP</li>
-                <li>Pandas, TensorFlow</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-xl mb-2">Soft Skills</h3>
-              <ul className="list-disc list-inside">
-                <li>Communication</li>
-                <li>Teamwork</li>
-                <li>Problem-Solving</li>
-                <li>Project Management</li>
-              </ul>
-              <h3 className="text-xl mt-4 mb-2">Design Skills</h3>
-              <ul className="list-disc list-inside">
-                <li>Photoshop, Figma</li>
-                <li>Wireframing</li>
-              </ul>
-            </div>
-          </div>
-        </section>
-      ) : page === "My Stuffs" ? (
-        <div>
-          <div className="grid sm:grid-cols-2 gap-2">
-            {githubRepos.map((repo, index) => (
-              <ProjectCard key={index} repo={repo} />
-            ))}
-          </div>
-        </div>
-      ) : page === "Resume" ? (
-        <main className="border-0 flex w-full justify-center opacity-75 mt-2 text-sm">
-          too bored to make a resume.
-        </main>
-      ) : (
-        "404 not found"
-      )}
-    </main>
+    <main className="h-[100vh] w-full ml-2.5 mt-2">{renderPageContent()}</main>
   );
-}
+};
 
 export default AboutMe;
